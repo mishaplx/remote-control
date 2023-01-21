30931b7fe57ced9188caba5fd7fc91348adc66c4
plehnevichma <plehnevichma@solit.by>
2023-01-21 21:52:14 +0300

    readme

--- src/tools/mousePos.ts
+++
@@ -1,8 +0,0 @@
-
-export default function mousePos(ws,mouseClass) {
-    console.log('mousePos');
-    mouseClass.getPosition().then((r) => {
-        console.log(r)
-        ws.send(JSON.stringify(r))
-    })
-}
